5d3dac38e7168b4317f1113e9fb7148893491cec
macbf <macbf0@gmail.com>
2020-04-10 15:30:40 -0300

    File Stack.py inserted

---
+++ Stack.py
@@ -0,0 +1,32 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+    
+    def insert(self, x):
+        self.stack.append(x)
+    
+    def remove(self):
+        if(self.stack != []):
+            self.stack.pop(len(self.stack) - 1)
+
+    def length(self):
+        return(print(len(self.stack)))
+    
+    def empty(self):
+        if(self.stack == []):
+            print('Vazio')
+        print('Não Vazio')
+
+    def head(self):
+        return print(self.stack[-1])
+
+
+pilha = Stack()
+pilha.insert(5)
+pilha.insert(7)
+pilha.empty()
+
+pilha.length()
+pilha.remove()
+pilha.head()
